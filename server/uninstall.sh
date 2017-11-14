fa0e97b2df993501cf82cb1988db962c33d09e2a
Ubuntu <nikolasdn@UbuntuTest.y2ognm2ax2yedllvzdtqifzbmf.ax.internal.cloudapp.net>
2017-11-14 12:24:18 +0000

    removed all .net shit

--- server/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sfctl application delete --application-id server
-sfctl application unprovision --application-type-name serverType --application-type-version 1.0.0
-sfctl store delete --content-path server
